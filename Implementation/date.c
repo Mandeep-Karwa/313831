e472cd568cbcc4a87f1190130347938df0d7a32e
Mandeep-Karwa <mandeepkarwa9@gmail.com>
2021-07-13 15:52:01 +0530

    commit

--- Implementation/date.c
+++ Implementation/date.c
@@ -6,30 +6,15 @@ int date(int yy,int mm,int dd)
     int correct_date;
     time_t T= time(NULL);
     struct  tm tm = *localtime(&T);//stores the present date and time in the tm variable of data type tm
-    if(yy>tm.tm_year+1900)//check whether the year given is more than the present year 
-        {
-            correct_date=-1;
-            printf("You have entered the wrong date\n");
-            return correct_date;;
-        }
-        else
-        { 
-            if(mm>tm.tm_mon+1&&yy==tm.tm_year+1900)//checks whether the month is more than the present month or not
-            {
-                correct_date=-1;
-                printf("You have entered the wrong date\n");
-                return correct_date;;
-            }
-            else
-            {
-                if(mm==tm.tm_mon+1&&yy==tm.tm_year&&dd>tm.tm_mday)//checks whether the date is more than the present date or not
-                {
-                    correct_date=-1;
-                    printf("You have entered the wrong date\n");
-                    return correct_date;;
-                }
-            }
-        }
+    int sp,sg,v;
+    sp=((yy*100)+mm)*100+dd;//givent date
+    sg=((((tm.tm_year+1900)*100)+(tm.tm_mon+1))*100)+tm.tm_mday;//present day 
+    if(sg-sp>0)//checks whether the date given is future or not
+    {
+        correct_date=-1;
+        printf("You have entered the wrong date\n");
+        return correct_date;
+    }
         if(mm==2&&dd<=29&&dd>=1&&yy%4==0)//checking for leap year condition date
         {
                 correct_date=0;
