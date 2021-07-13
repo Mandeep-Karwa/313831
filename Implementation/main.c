8bcb9f67bdb7ac9e7d9b21ccce14eb6a22cee944
Mandeep-Karwa <mandeepkarwa9@gmail.com>
2021-07-13 13:51:04 +0530

    commit

--- Implementation/main.c
+++ Implementation/main.c
@@ -6,9 +6,7 @@ int main()
 {
     int selected_no,options;
     char username[1000];
-    selected_no=0;
     printf("Please select the option which you want to choose\n1. Signin to your account\n2. Create an account\n");
-    // print options as given as in switch statement
     scanf("%d",&selected_no);
     for(options=3;options>0;options--)
     {
@@ -29,14 +27,11 @@ int main()
         printf("You are left with only one option that is write in your databse\n1.Write your first entry in your databse\nIf you select other options you will be logged out\n");
         scanf("%d",&selected_no);
         if(selected_no==1)
-        {
             write(username);
-        }
         else
         {
             goto shutdown;
         }
-        exit(0);
     }
     if(options==0)
     {
